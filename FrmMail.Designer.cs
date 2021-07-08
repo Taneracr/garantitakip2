727f2564bdae56990d4863ac59d2ceee40b1f889
Taner <82117161+Taneracr@users.noreply.github.com>
2021-07-08 17:28:19 +0300

    Taner-08-07-2021-MailGonderme

---
+++ FrmMail.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace garantiTakip
+{
+    partial class FrmMail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kime :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Doğum Günü Gelenler  :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(176, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 24);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(176, 29);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 24);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(222, 26);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(183, 24);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Garanti Bitimi Yaklaşan :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(277, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 48);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Gönder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.comboBox4);
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(441, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(454, 500);
+            this.panel1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Kime :";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(222, 80);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(183, 24);
+            this.comboBox4.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightCoral;
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(442, 500);
+            this.panel2.TabIndex = 13;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 184);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 196);
+            this.listBox1.TabIndex = 9;
+            // 
+            // FrmMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 500);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmMail";
+            this.Text = "FrmMail";
+            this.Load += new System.EventHandler(this.FrmMail_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ FrmMail.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace garantiTakip
+{
+    public partial class FrmMail : Form
+    {
+        public FrmMail()
+        {
+            InitializeComponent();
+        }
+        stajyerEntities3 db = new stajyerEntities3();
+
+        // taner811907 computer190781
+        // qwerty1907 acar1907881
+
+
+
+        public void MailGonder()
+        {
+
+            MailMessage mesajım = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+            istemci.Credentials = new System.Net.NetworkCredential("computer190781@gmail.com", "taner811907");
+            istemci.Port = 587;
+            istemci.Host = "smtp.gmail.com";
+            istemci.EnableSsl = true;
+            mesajım.To.Add(comboBox1.Text);
+            mesajım.From = new MailAddress(comboBox1.Text);
+            mesajım.Subject = "Kodla Teknoloji";
+            mesajım.Body = "Doğum Gününüz Kutlu Olsun";
+            istemci.Send(mesajım);
+
+        }
+
+        public void GarantiMailGonder()
+        {
+
+            MailMessage mesajım = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+            istemci.Credentials = new System.Net.NetworkCredential("computer190781@gmail.com", "taner811907");
+            istemci.Port = 587;
+            istemci.Host = "smtp.gmail.com";
+            istemci.EnableSsl = true;
+            mesajım.To.Add(comboBox4.Text);
+            mesajım.From = new MailAddress(comboBox4.Text);
+            mesajım.Subject = "Kodla Teknoloji";
+            mesajım.Body = "Garantinizin Bitimine 10 Gün Kalmıştır";
+            istemci.Send(mesajım);
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailGonder();
+        }
+
+        private void FrmMail_Load(object sender, EventArgs e)
+        {
+            //comboBox1.Items.Add("computer190781@gmail.com");
+
+            //foreach (var item in db.tbl_Yetkili)
+            //{                             
+            //    comboBox1.Items.Add(item.MAIL);
+            //}
+
+            foreach (var item in db.tbl_Yetkili)
+            {
+                DateTime gun = Convert.ToDateTime(item.DGMTARİH);
+                int guun = gun.Day;
+                int ay = gun.Month;
+
+                if (guun.ToString() == DateTime.Now.Date.Day.ToString() && ay.ToString() == DateTime.Now.Date.Month.ToString())
+                {
+                    comboBox2.Items.Add(item.AD + ": " + item.DGMTARİH);
+                    comboBox1.Items.Add(item.MAIL);
+
+                    //  MailGonder(comboBox1.Items[0].ToString(), comboBox1.Items[0].ToString());
+                   // MailGonder(comboBox1.Items[3].ToString(), comboBox1.Items[3].ToString());
+                    
+
+                    //MailGonder(); // doğum günü Maili
+                    
+                }
+            }
+
+
+
+            int bugun = Convert.ToInt32(DateTime.Now.Date.Day);
+            int bugunay = Convert.ToInt32(DateTime.Now.Date.Month);
+
+            foreach (var item in db.tbl_cari)
+            {
+                DateTime gun = Convert.ToDateTime(item.tbl_baslangicBitisTarih.BİTİSTARİH);
+                int guun = gun.Day;
+                int ay = gun.Month;
+                int islem = guun - bugun;
+
+                if (bugunay == ay && islem == 10)
+                {
+                    comboBox3.Items.Add(item.tbl_Yetkili.AD + ": " + item.tbl_baslangicBitisTarih.BİTİSTARİH);
+                    comboBox4.Items.Add(item.tbl_Yetkili.MAIL);
+
+                }
+
+                //comboBox4.Items.Add(db.tbl_cari.Select(x => x.tbl_Yetkili.MAIL).ToString());
+                // GarantiMailGonder(); // Garanti Bitimine 10 gün kala Maili
+
+
+                // comboBox3.Items.Add(item.BİTİSTARİH);
+            }
+
+
+
+
+
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GarantiMailGonder();
+        }
+    }
+}
